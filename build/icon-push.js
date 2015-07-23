86f8d7d4f669b8c5393fcd04526935b104550de6
Tony <tonylefler@gmail.com>
2015-07-23 11:58:18 -0600

    add icon injection directive

--- build/icon-push.js
+++ build/icon-push.js
@@ -6,4 +6,15 @@ angular.module('iconPush', [])
             replace: true,
             template: '-svg-'
         }
+    })
+
+    .directive('icon', function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            link: function(scope, element,attrs) {
+                scope.class = attrs.class;
+            },
+            template: '<svg role="img" class="{{class}}"><use xlink:href="#{{class}}"></use></svg>'
+        }
     });
